15ec4d46223a651414696cecea76608db90792db
haruki-m <Haruki.Mikami@partner.ibm.com>
2025-08-23 18:19:27 +0900

    連絡詳細画面のフォーム周りの実装

--- app/routes/contacts/detail.tsx
+++ app/routes/contacts/detail.tsx
@@ -6,7 +6,6 @@ import { CheckedMark } from "~/shared/components/checked-mark/checked-mark";
 import { Input } from "~/shared/components/input/input";
 import { CalendarInput } from "~/shared/components/calendar-input/calendar-input";
 import { useState } from "react";
-import { parseDate, type CalendarDate } from "@internationalized/date";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,13 +13,21 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+/** 連絡詳細画面 */
 export default function Detail({ params }: Route.LoaderArgs) {
-  const [date, setDate] = useState<string>('test');
+  const [detentionDate, setDetentionDate] = useState<string>();
+  const [electionDate, setElectionDate] = useState<string>();
   const [detentionDateShown, setDetentionDateShown] = useState<boolean>(false);
+  const [electionDateShown, setElectionDateShown] = useState<boolean>(false);
+
+  /** 全てのカレンダーポップアップ画面を閉じる */
+  const closeAllCalendars = () => {
+    setDetentionDateShown(false);
+    setElectionDateShown(false);
+  };
 
   return (
     <main className='flex flex-col w-full md:w-[73%] py-8 md:pl-8 md:pr-4 h-full'>
-      {/* <h1>Detail id : {params.id}</h1> */}
       <header className='text-center'>
         <h1 className='text-[32px] leading-[150%]'>被疑者国選弁護報告</h1>
       </header>
@@ -36,48 +43,57 @@ export default function Detail({ params }: Route.LoaderArgs) {
                   <RequirementBadge>※</RequirementBadge>
                   <CheckedMark isChecked={true} />
                 </Label>
-                <Input name='name' blockSize='md' autoComplete='true' className='w-135 mt-3' required />
+                <Input name='name' blockSize='md' className='w-135 mt-3' required />
                 {/* 事件番号 */}
                 <Label size='md' htmlFor='incidentNo' className='mt-6'>事件番号
                   <RequirementBadge>※</RequirementBadge>
                   <CheckedMark isChecked={true} />
                 </Label>
-                <Input type='number' name='incidentNo' blockSize='md' autoComplete='true'
+                <Input type='number' name='incidentNo' blockSize='md'
                   className='w-60 mt-3 mx-0 mb-0
                   [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none
-                  [&::-webkit-inner-spin-button]:appearance-none' required />
+                  [&::-webkit-inner-spin-button]:appearance-none text-right' required />
                 <span className='ml-2'>年</span>
-                <Input type='number' name='incidentNo' blockSize='md' autoComplete='true'
+                <Input type='number' name='incidentNo' blockSize='md'
                   className='w-60 mt-3 mr-0 mb-0 ml-4
                   [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none
-                  [&::-webkit-inner-spin-button]:appearance-none' required />
+                  [&::-webkit-inner-spin-button]:appearance-none text-right' required />
                 <span className='ml-2'>号</span>
-                {/* 勾留日 TODO : カレンダー入力に修正 */}
+                {/* 勾留日 */}
                 <Label size='md' htmlFor='detentionDate' className='mt-6'>勾留日
                   <RequirementBadge>※</RequirementBadge>
                   <CheckedMark isChecked={false} />
                 </Label>
                 <div className='relative'>
-                  <Input name='detentionDate' blockSize='md' autoComplete='true' className='w-135 mt-3 text-right' value={date} 
-                    onClick={() => setDetentionDateShown(!detentionDateShown)} required readOnly />
-                  <CalendarInput className='absolute top-16 right-0' isShown={detentionDateShown} onChange={(val) => { setDate(val.toString()) }} />
+                  <Input name='detentionDate' blockSize='md' className='w-135 mt-3 text-right cursor-pointer' value={detentionDate}
+                    onClick={() => setDetentionDateShown(true)} required readOnly />
+                  <CalendarInput className='absolute top-18 right-0 z-1' isShown={detentionDateShown}
+                    onChange={(val) => { setDetentionDate(val.toString()); setDetentionDateShown(false); }} />
                 </div>
-                {/* 選任日 TODO : カレンダー入力に修正 */}
+                {/* 選任日 */}
                 <Label size='md' htmlFor='electionDate' className='mt-6'>選任日
                   <RequirementBadge>※</RequirementBadge>
                   <CheckedMark isChecked={false} />
                 </Label>
-                <Input name='electionDate' blockSize='md' autoComplete='true' className='w-135 mt-3' required />
+                <div className='relative'>
+                  <Input name='electionDate' blockSize='md' className='w-135 mt-3 text-right cursor-pointer' value={electionDate}
+                    onClick={() => setElectionDateShown(true)} required readOnly />
+                  <CalendarInput className='absolute top-18 right-0 z-1' isShown={electionDateShown}
+                    onChange={(val) => { setElectionDate(val.toString()); setElectionDateShown(false); }} />
+                </div>
                 {/* 罪名 */}
                 <Label size='md' htmlFor='crimeName' className='mt-6'>罪名
                   <RequirementBadge>※</RequirementBadge>
                   <CheckedMark isChecked={false} />
                 </Label>
-                <Input name='crimeName' blockSize='md' autoComplete='true' className='w-135 mt-3' required />
+                <Input name='crimeName' blockSize='md' className='w-135 mt-3' required />
               </form>
             </AccordionContent>
           </Accordion>
       </section>
+      {/* オーバーレイ */}
+      <div className={`fixed top-0 left-0 w-full h-full ${detentionDateShown || electionDateShown ? 'block' : 'hidden'}`}
+        onClick={closeAllCalendars} />
     </main>
   );
 }
